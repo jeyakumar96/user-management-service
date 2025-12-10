5bbb9e78612545d11c48ebf1224bef4c1a629f65
JeyakumarMoorthy <JeyakumarMoorthy@KANINI-LTP-764>
2025-12-10 18:23:42 +0530

    create login and register component and set routes

--- auth-ui-angular/src/app/app.routes.ts
+++ auth-ui-angular/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './component/login/login';
+import { Register } from './component/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'login' },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: '**', redirectTo: 'login' }
+];
