149c8aa5191d54a8d6cc5941ea33da4112de9c0e
JeyakumarMoorthy <JeyakumarMoorthy@KANINI-LTP-764>
2025-12-13 22:25:07 +0530

    add logout function

--- auth-ui/src/app/shared/components/header/header.ts
+++ auth-ui/src/app/shared/components/header/header.ts
@@ -1,8 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, RouterModule, MatButtonModule, MatIconModule, MatDialogModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
@@ -17,9 +23,13 @@ export class Header {
 
   showAppsMenu = false;
 
-  onAppsMenuClick() {
-    this.showAppsMenu = !this.showAppsMenu;
-    this.appsMenuToggle.emit();
+  constructor(private authService: AuthService, private router: Router, public dialog: MatDialog) {}
+
+
+
+    logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
 
